abf05af9b5a56086e08210ca9dd66741e67620d2
subhasis-karmakar <subhasis18.karmakar@gmail.com>
2026-04-15 11:49:32 +0530

    Fix dependency compatibility for CI

--- infra/modules/app_insights/main.tf
+++ infra/modules/app_insights/main.tf
@@ -2,7 +2,8 @@ resource "azurerm_application_insights" "this" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  application_type    = "web"
+  application_type    = var.application_type
   workspace_id        = var.workspace_id
-  tags                = var.tags
+
+  tags = var.tags
 }
